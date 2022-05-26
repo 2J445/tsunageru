bbe515423a4ae8db94d2c35b8f6d22a26a718194
Ubuntu <ubuntu@ip-172-31-45-102.ap-northeast-1.compute.internal>
2022-05-26 06:25:21 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :edit]
   def index
     @pagy, @users = pagy(User.order(id: :desc), items: 25)
   end
@@ -15,27 +16,32 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to '/'
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
   end
-
+  
   def edit
   end
-
+  
   def update
   end
-
+  
   def destroy
   end
   
   private
-
+  
+  def set_image
+      @image = Image.find(params[:id])
+  end
+    
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :self_introduction)
   end
   
 end
